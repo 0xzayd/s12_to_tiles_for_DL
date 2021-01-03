9fc307952c7f792439badf261cf58bb50c48e1aa
0xzayd <zayd@theta.space>
2021-01-03 15:14:39 +0100

    subprocess because SNAP doesn't free memory

--- api/src/S1Processor.py
+++ api/src/S1Processor.py
@@ -1,9 +1,3 @@
-# extended from https://github.com/wajuqi/Sentinel-1-preprocessing-using-Snappy
-
-import snappy
-from snappy import ProductIO
-from snappy import HashMap
-from snappy import GPF
 import os
 import zipfile
 import logging
@@ -11,6 +5,7 @@ import glob
 from src.Processor import Processor
 import numpy as np
 import boto3
+import subprocess
 
 ACCESS_KEY = ''
 SECRET_KEY = ''  #old ones not working anyways
@@ -55,13 +50,6 @@ class S1Processor(Processor):
                 f.extractall(self.zips_path)   
 
 
-    def apply_orbit_file(self, source):
-        logger.info('\tApplying orbit file')
-        parameters = HashMap()
-        parameters.put('Apply-Orbit-File', True)
-        output = GPF.createProduct('Apply-Orbit-File', parameters, source)
-        return output
-
     def get_meta(self):
         for i in range(len(self.safe_folders)):
             only_safe_folder = os.path.basename(self.safe_folders[i])
@@ -84,81 +72,6 @@ class S1Processor(Processor):
                 self.pols.append('NaN')
                 logger.info("Polarization error!")
 
-    def remove_thermal_noise(self, source):
-        logger.info('\tThermal noise removal')
-        parameters = HashMap()
-        parameters.put('removeThermalNoise', True)
-        output = GPF.createProduct('ThermalNoiseRemoval', parameters, source)
-        return output
-
-    def calibrate(self, source, pol, polarization):
-        logger.info('\tCalibration')
-        parameters = HashMap()
-        parameters.put('outputSigmaBand', True)
-
-        if polarization == 'DH':
-            parameters.put('sourceBands', 'Intensity_HH,Intensity_HV')
-        elif polarization == 'DV':
-            parameters.put('sourceBands', 'Intensity_VH,Intensity_VV')
-        elif polarization == 'SH' or polarization == 'HH':
-            parameters.put('sourceBands', 'Intensity_HH')
-        elif polarization == 'SV':
-            parameters.put('sourceBands', 'Intensity_VV')
-        else:
-            logger.info("Unknown polarization")
-
-        parameters.put('selectedPolarisations', pol)
-        parameters.put('outputImageScaleInDb', False)
-        parameters.put('auxFile', 'Product Auxiliary File')
-        parameters.put('outputImageInComplex', False)
-        parameters.put('outputGammaBand', False)
-        parameters.put('outputBetaBand', False)
-        output = GPF.createProduct("Calibration", parameters, source)
-        return output
-
-
-    def multi_temporal_despeckling(self):
-        pass
-        return
-
-
-    def terrain_correction(self, source):
-        logger.info('\tTerrain correction...')
-        parameters = HashMap()
-        parameters.put('demName', 'External DEM')
-        parameters.put('externalDEMFile', './Mosaic_W_EUR.tif')
-        parameters.put('imgResamplingMethod', 'BILINEAR_INTERPOLATION')
-        #parameters.put('mapProjection', 'AUTO:42001')       # comment this line if no need to convert to UTM/WGS84, default is WGS84
-        parameters.put('saveProjectedLocalIncidenceAngle', True)
-        parameters.put('saveSelectedSourceBand', True)
-        #parameters.put('pixelSpacingInMeter', 10.0)
-        #parameters.put('pixelSpacingInDegree', 8.983152841195215E-5)
-        #parameters.put('alignToStandardGrid', False)
-        #parameters.put('standardGridOriginX', 0.0)
-        #parameters.put('standardGridOriginXY', 0.0)
-        parameters.put('nodataValueAtSea', True)
-        #parameters.put('saveDEM', False)
-        parameters.put('saveSelectedSourceBand', True)
-        parameters.put('incidenceAngleForSigma0', 'Use projected local incidence angle from DEM')
-        #parameters.put('auxFile', 'Latest Auxiliary File')
-        
-        output = GPF.createProduct('Terrain-Correction', parameters, source)
-        return output
-
-    def subset(self, source):
-        logger.info('\tClipping to AOI')
-        parameters = HashMap()
-        parameters.put('geoRegion', self.footprint)
-        output = GPF.createProduct('Subset', parameters, source)
-        return output
-
-
-    def scale_db(self, source):
-        logger.info('\tScaling to dB')
-        parameters = HashMap()
-        parameters.put('sourceBands', 'Sigma0_VV,Sigma0_VH')
-        output = GPF.createProduct("LinearToFromdB", parameters, source)
-        return output
 
     def process(self):
 
@@ -168,25 +81,13 @@ class S1Processor(Processor):
 
         for i, safe_folder in enumerate(self.safe_folders):
        
-            scene = ProductIO.readProduct(safe_folder + '/manifest.safe')   
-            applyorbit = self.apply_orbit_file(scene)
-            thermaremoved = self.remove_thermal_noise(applyorbit)
-            calibrated = self.calibrate(thermaremoved, self.pols[i], self.polarizations[i])
-            tercorrected = self.terrain_correction(calibrated)
-
-            # subset here
-            if self.footprint:
-                tercorrected = self.subset(tercorrected)
-
-            scaled_db = self.scale_db(tercorrected)
-            
-            print('writing')
+            pol = self.pols[i]
+            polarization = self.polarizations[i]
+            footprint = self.footprint
             output_path = os.path.join(self.zips_path, self.basenames[i]) + '_VV_VH_dB.tif'
-            ProductIO.writeProduct(scaled_db, output_path, 'GeoTIFF-BigTIFF')
-            print('finished writing')
-            scene.dispose()
-            scene.closeIO()
-            #self.paths_to_merge.append(output_path)
+
+            subprocess.call(['python', 'subp.py', pol, polarization, footprint, output_path])
+            
 
         logger.info('merging now S1 scenes')
         self.paths_to_merge = glob.glob(os.path.join(self.zips_path, '*.tif'))
---
+++ api/src/subp.py
@@ -0,0 +1,107 @@
+import sys
+#import snappy
+from snappy import ProductIO
+from snappy import HashMap
+from snappy import GPF
+
+pol = sys.argv[1]
+polarization = sys.argv[2]
+footprint = sys.argv[3]
+output_path = sys.argv[4]
+
+
+def apply_orbit_file(source):
+    logger.info('\tApplying orbit file')
+    parameters = HashMap()
+    parameters.put('Apply-Orbit-File', True)
+    output = GPF.createProduct('Apply-Orbit-File', parameters, source)
+    return output
+
+
+def remove_thermal_noise(source):
+    logger.info('\tThermal noise removal')
+    parameters = HashMap()
+    parameters.put('removeThermalNoise', True)
+    output = GPF.createProduct('ThermalNoiseRemoval', parameters, source)
+    return output
+
+
+def calibrate(source, pol, polarization):
+    logger.info('\tCalibration')
+    parameters = HashMap()
+    parameters.put('outputSigmaBand', True)
+
+    if polarization == 'DH':
+        parameters.put('sourceBands', 'Intensity_HH,Intensity_HV')
+    elif polarization == 'DV':
+        parameters.put('sourceBands', 'Intensity_VH,Intensity_VV')
+    elif polarization == 'SH' or polarization == 'HH':
+        parameters.put('sourceBands', 'Intensity_HH')
+    elif polarization == 'SV':
+        parameters.put('sourceBands', 'Intensity_VV')
+    else:
+        logger.info("Unknown polarization")
+
+    parameters.put('selectedPolarisations', pol)
+    parameters.put('outputImageScaleInDb', False)
+    parameters.put('auxFile', 'Product Auxiliary File')
+    parameters.put('outputImageInComplex', False)
+    parameters.put('outputGammaBand', False)
+    parameters.put('outputBetaBand', False)
+    output = GPF.createProduct("Calibration", parameters, source)
+    return output
+
+
+def terrain_correction(source):
+    logger.info('\tTerrain correction...')
+    parameters = HashMap()
+    parameters.put('demName', 'External DEM')
+    parameters.put('externalDEMFile', './Mosaic_W_EUR.tif')
+    parameters.put('imgResamplingMethod', 'BILINEAR_INTERPOLATION')
+    #parameters.put('mapProjection', 'AUTO:42001')       # comment this line if no need to convert to UTM/WGS84, default is WGS84
+    parameters.put('saveProjectedLocalIncidenceAngle', True)
+    parameters.put('saveSelectedSourceBand', True)
+    parameters.put('nodataValueAtSea', True)
+    parameters.put('saveSelectedSourceBand', True)
+    parameters.put('incidenceAngleForSigma0', 'Use projected local incidence angle from DEM')
+    
+    output = GPF.createProduct('Terrain-Correction', parameters, source)
+    return output
+
+def subset(source, footprint):
+    logger.info('\tClipping to AOI')
+    parameters = HashMap()
+    parameters.put('geoRegion', footprint)
+    output = GPF.createProduct('Subset', parameters, source)
+    return output
+
+
+def scale_db(source):
+    logger.info('\tScaling to dB')
+    parameters = HashMap()
+    parameters.put('sourceBands', 'Sigma0_VV,Sigma0_VH')
+    output = GPF.createProduct("LinearToFromdB", parameters, source)
+    return output
+
+
+def main(pol, polarization, footprint, output_path):
+    scene = ProductIO.readProduct(safe_folder + '/manifest.safe')   
+    applyorbit = apply_orbit_file(scene)
+    thermaremoved = remove_thermal_noise(applyorbit)
+    calibrated = calibrate(thermaremoved, pol, polarization)
+    tercorrected = terrain_correction(calibrated)
+
+    # subset here
+    if footprint:
+        tercorrected = subset(tercorrected)
+
+    scaled_db = scale_db(tercorrected)
+    
+    print('writing')
+    ProductIO.writeProduct(scaled_db, output_path, 'GeoTIFF-BigTIFF')
+    print('finished writing')
+    scene.dispose()
+    scene.closeIO()
+
+
+main(pol, polarization, footprint, output_path)
